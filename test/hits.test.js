acae25a9a42feb2678a6fb3dfe0e3f8c2ea28d84
Lili Boxer <ninaliboxer@gmail.com>
2019-07-29 17:09:36 -0700

    refactored to use flatmap for created hits

---
+++ test/hits.test.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const { getAgent } = require('./data-helpers');
+
+describe('It is our Hits test', () => {
+  it('GET top 10 sites', () => {
+    return getAgent()
+      .get('/hits/top10')
+      .then(res => {
+        console.log(res.body);
+        expect(res.body).toHaveLength(9);
+        expect(res.body[0].hitCount).toBeGreaterThan(res.body[8].hitCount);
+      });
+  });
+});
--- test/seed-data.js
+++ test/seed-data.js
@@ -1,8 +1,9 @@
 const User = require('../lib/models/User');
 const URL = require('../lib/models/URL');
+const Hit = require('../lib/models/Hit');
 const chance = require('chance').Chance();
 
-module.exports = async({ users = 12, URLs = 10 } = { users: 12, URLs: 10 }) => {
+module.exports = async({ users = 12, URLs = 10, hits = 50 } = { users: 12, URLs: 10, hits: 50 }) => {
   const createdUsers = await User.create(
     [...Array(users)].map(() => ({
       username: chance.name(),
@@ -22,8 +23,20 @@ module.exports = async({ users = 12, URLs = 10 } = { users: 12, URLs: 10 }) => {
     }))
   );
 
+  const createdHits = await Hit
+    .create(createdURLs.flatMap(URL => {
+      return [...Array(chance.integer({ min: 1, max: hits }))]
+        .map(() => ({
+          URL: URL._id,
+          ip: chance.ip(),
+        }));
+    }));
+    
+  
+
   return {
     users: createdUsers,
     URLs: [userURL, ...createdURLs],
+    hits: createdHits,
   };
 };

13c199a9a2546bbb083a7a3e0f7a1e45d78dc4dd
Lili Boxer <ninaliboxer@gmail.com>
2019-07-30 17:37:26 -0700

    updating tests to pass travis

--- test/hits.test.js
+++ test/hits.test.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
-const { getAgent, getHits } = require('./data-helpers');
+const { getAgent } = require('./data-helpers');
 
 describe('It is our Hits test', () => {
   it('GET top 10 sites', () => {
     return getAgent()
       .get('/hits/top10')
       .then(res => {
-        expect(res.body).toHaveLength(9);
-        expect(res.body[0].hitCount).toBeGreaterThan(res.body[8].hitCount);
+        expect(res.body[0].hitCount).toBeGreaterThan(res.body[9].hitCount);
+        expect(res.body).toHaveLength(10);
       });
   });
 });
